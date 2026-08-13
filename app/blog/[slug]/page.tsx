875b951977abde14a0ade7fbc2335fe97c9caf51
Rock-Astley <rockastley3000@gmail.com>
2026-08-13 06:49:16 +0530

    changes

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -32,15 +32,61 @@ export default async function Post({ params }: Props) {
 
   return (
     <main className="min-h-screen bg-black text-white p-6">
-      <h1 className="text-4xl neon-blue mb-4">
-        {data?.title}
-      </h1>
+      <div className="max-w-3xl mx-auto">
 
-      <div
-        dangerouslySetInnerHTML={{
-          __html: processed.toString(),
-        }}
-      />
+        {/* Title */}
+        <h1 className="text-4xl font-bold neon-blue mb-8">
+          {data?.title}
+        </h1>
+
+        {/* Post content */}
+        <article
+          className="
+            text-gray-200
+            leading-7
+
+            [&_p]:mb-6
+
+            [&_h1]:text-3xl
+            [&_h1]:font-bold
+            [&_h1]:mb-6
+
+            [&_h2]:text-2xl
+            [&_h2]:font-bold
+            [&_h2]:mb-4
+            [&_h2]:mt-8
+
+            [&_h3]:text-xl
+            [&_h3]:font-bold
+            [&_h3]:mb-3
+            [&_h3]:mt-6
+
+            [&_ul]:list-disc
+            [&_ul]:ml-6
+            [&_ul]:mb-6
+
+            [&_ol]:list-decimal
+            [&_ol]:ml-6
+            [&_ol]:mb-6
+
+            [&_li]:mb-2
+
+            [&_blockquote]:border-l-2
+            [&_blockquote]:border-cyan-400
+            [&_blockquote]:pl-4
+            [&_blockquote]:my-6
+
+            [&_a]:text-cyan-400
+            [&_a]:underline
+
+            [&_strong]:font-bold
+          "
+          dangerouslySetInnerHTML={{
+            __html: processed.toString(),
+          }}
+        />
+
+      </div>
     </main>
   );
 }
